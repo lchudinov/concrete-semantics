86eb0ebf3020ad1e3b8f4b00e65436bfbc2a9721
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-03-22 08:45:16 +0500

    Up to exercise 2.3

--- MyList.thy
+++ MyList.thy
@@ -37,5 +37,22 @@ theorem rev_rev [simp]: "rev(rev xs) = xs"
   apply (auto)
   done
 
+fun map :: "('a \<Rightarrow> 'b) \<Rightarrow> 'a list \<Rightarrow> 'b list" where
+"map f Nil = Nil" |
+"map f (Cons x xs) = Cons (f x) (map f xs)"
+
+(* Exercise 2.1 *)
+value "1 + (2::nat)"
+value "1 + (2::int)"
+value "1 - (2::nat)"
+value "1 - (2::int)"
+
+(* Exercise 2.3 *)
+(*
+fun count :: "'a \<Rightarrow> 'a list \<Rightarrow> nat" where
+"count x Nil = 0" |
+"count x (Cons x xs) = if (x  x0) then 1 + (count x xs)" else count x xs"
+*)
+
 end
 
--- add.thy
+++ add.thy
@@ -6,11 +6,37 @@ fun add :: "nat => nat => nat" where
 "add 0 n = n" |
 "add (Suc m) n = Suc (add m n)"
 
-lemma add_02 : "add m 0 = m"
+lemma add_02 [simp] : "add m 0 = m"
 apply (induction m)
 apply (auto)
 done
 
 thm add_02
+
+(* Exercise 2.2 *)
+
+lemma add_succ [simp] : "add n (Suc m) = Suc (add n m)"
+  apply (induction n)
+  apply (auto)
+  done
+
+theorem add_comm [simp] : "add m n = add n m"
+  apply (induction m)
+  apply (auto)
+  done
+  
+theorem add_assoc [simp] : "add (add m n) k = add m (add n k)"
+  apply (induction m)
+  apply (auto)
+  done
+
+fun double :: "nat \<Rightarrow> nat" where
+"double 0 = 0" |
+"double (Suc x) = Suc (Suc (double x))"
+
+theorem double_correct [simp] : "double m = add m m"
+  apply (induction m)
+  apply (auto)
+  done
 
 end
