0daffd1fa57b45e967b588e70d552b881f88b9f8
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-04-06 10:23:00 +0500

    Up to 3.8

---
+++ BExp.thy
@@ -0,0 +1,97 @@
+theory BExp
+imports AExp
+begin
+declare [[names_short]]
+
+datatype bexp = Bc bool | Not bexp | Andb bexp bexp | Less aexp aexp
+
+fun bval :: "bexp \<Rightarrow> state \<Rightarrow> bool" where
+"bval (Bc v) s = v" |
+"bval (Not b) s = (\<not> bval b s)" |
+"bval (Andb b1 b2) s = (bval b1 s \<and> bval b2 s)" |
+"bval (Less a1 a2) s = (aval a1 s < aval a2 s)"
+
+fun not :: "bexp \<Rightarrow> bexp" where
+"not (Bc True) = Bc False" |
+"not (Bc False) = Bc True" |
+"not b = Not b"
+
+fun "and" :: "bexp \<Rightarrow> bexp \<Rightarrow> bexp" where
+"and (Bc True) b = b" |
+"and b (Bc True) = b" |
+"and (Bc False) b = Bc False" |
+"and b (Bc False) = Bc False" | 
+"and b1 b2 = Andb b1 b2"
+
+fun less :: "aexp \<Rightarrow> aexp \<Rightarrow> bexp" where
+"less (N n1) (N n2) = Bc (n1 < n2)" |
+"less a1 a2 = Less a1 a2"
+
+fun bsimp :: "bexp \<Rightarrow> bexp" where
+"bsimp (Bc v) = Bc v" |
+"bsimp (Not b) = not (bsimp b)" |
+"bsimp (Andb b1 b2) = and (bsimp b1) (bsimp b2)" |
+"bsimp (Less a1 a2) = less (asimp a1) (asimp a2)"
+
+(* Exercise 3.7. *)
+fun Eq :: "aexp \<Rightarrow> aexp \<Rightarrow> bexp" where
+"Eq a1 a2 = (Andb (Not (Less a1 a2)) (Not (Less a2 a1)))"
+
+fun Le :: "aexp \<Rightarrow> aexp \<Rightarrow> bexp" where
+"Le a1 a2 = (Not (Less a2 a1))"
+
+lemma "bval (Eq a1 a2) s = (aval a1 s = aval a2 s)"
+  apply (induction a1)
+  apply (auto)
+  done
+
+lemma "bval (Le a1 a2) s = (aval a1 s \<le> aval a2 s)"
+  apply (induction a1)
+  apply (auto)
+  done
+
+(* Exercise 3.8. *)
+datatype ifexp = Bc2 bool | If ifexp ifexp ifexp | Less2 aexp aexp
+
+fun ifval :: "ifexp \<Rightarrow> state \<Rightarrow> bool" where
+"ifval (Bc2 b)s = b" |
+"ifval (If i1 i2 i3) s = (if (ifval i1 s) then (ifval i2 s) else (ifval i3 s))" |
+"ifval (Less2 a1 a2) s = ((aval a1 s) < (aval a2 s))"
+
+fun orB :: "bexp \<Rightarrow> bexp \<Rightarrow> bexp" where
+"orB p q = Not (Andb (Not p) (Not q))"
+
+fun impliesB :: "bexp \<Rightarrow> bexp \<Rightarrow> bexp" where
+"impliesB p q = orB (Not p) q"
+
+fun IfB :: "bexp \<Rightarrow> bexp \<Rightarrow> bexp \<Rightarrow> bexp" where
+"IfB a b c = Andb (impliesB a b) (impliesB (Not a) c)"
+
+fun b2ifexp :: "bexp \<Rightarrow> ifexp" where
+"b2ifexp (Bc b) = (Bc2 b)" |
+"b2ifexp (Not b) = (If (b2ifexp b) (Bc2 False) (Bc2 True))" |
+"b2ifexp (Andb b1 b2) = (If (b2ifexp b1) (b2ifexp b2) (Bc2 False))" |
+"b2ifexp (Less a1 a2) = (Less2 a1 a2)"
+
+fun if2bexp :: "ifexp \<Rightarrow> bexp" where
+"if2bexp (Bc2 b) = Bc b" |
+"if2bexp (If i1 i2 i3) = IfB (if2bexp i1) (if2bexp i2) (if2bexp i3)" |
+"if2bexp (Less2 a1 a2) = (Less a1 a2)"
+
+value "if2bexp (If (Bc2 True) (Bc2 True) (Bc2 True))"
+
+lemma b2if_correct : "bval b s = ifval (b2ifexp b) s"
+  apply (induction b)
+  apply (auto)
+  done
+
+lemma if2bexp_correct : "ifval b s = bval (if2bexp b) s"
+  apply (induction b)
+  apply (auto)
+  done
+
+(* Exercise 3.9 *)
+datatype pbexp = VAR vname | NEG pbexp | AND pbexp pbexp | OR pbexp pbexp
+
+
+end
