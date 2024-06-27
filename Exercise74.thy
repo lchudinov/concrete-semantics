2cf8e9f9f73742203bf54ffef2497e10b135c944
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-06-27 08:27:53 +0500

    Exercise 7.4

--- Exercise74.thy
+++ Exercise74.thy
@@ -16,6 +16,34 @@ values "{c' | c'. (Plus (V ''x'') (V ''x''), <''x'' := 42>) \<leadsto> c'}"
 
 values "{c' | c'. (Plus (N 42) (V ''x''), <''x'' := 42>) \<leadsto> c'}"
 
+lemmas astep_induct = astep.induct[split_format(complete)]
+
+declare astep.intros[simp,intro]
+
+lemma "(a, s) \<leadsto> a' \<Longrightarrow> aval a s = aval a' s"
+proof (induction rule: astep_induct)
+qed auto+
+
+lemma "(a, s) \<leadsto> a' \<Longrightarrow> aval a s = aval a' s"
+proof (induction rule: astep_induct)
+  fix s x
+  show "aval (V x) s = aval (N (s x)) s" by simp
+next
+  fix i j s
+  show "aval (Plus (N i) (N j)) s = aval (N (i + j))s" by simp
+next
+  fix a1 s a1' a2
+  assume H1: "(a1, s) \<leadsto> a1'"
+  assume H2: "aval a1 s = aval a1' s"
+  show "aval (Plus a1 a2) s = aval (Plus a1' a2) s" using H1 H2 by simp
+next
+  fix a2 s a2' i
+  assume H1: "(a2, s) \<leadsto> a2'"
+  assume H2: " aval a2 s = aval a2' s"
+  show "aval (Plus (N i) a2) s = aval (Plus (N i) a2') s" using H1 H2 by simp
+qed
+
+
 
 
 
