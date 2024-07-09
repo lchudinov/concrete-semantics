da394fe2eaf39ee9cff4efcc6579e76a1917f742
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-07-09 08:45:57 +0500

    Up to 7.8

--- Exercise77.thy
+++ Exercise77.thy
@@ -1,3 +1,10 @@
-theory Exercise77 imports Big_Step
+theory Exercise77 imports Small_Step
 begin
+
+lemma
+ "\<lbrakk> C 0 = c;;d ; \<forall>n. (C n, S n) \<rightarrow> (C (Suc n), S (Suc n))\<rbrakk> \<Longrightarrow> (\<forall>n. \<exists> c1 c2. C n = c1;;d \<and> C (Suc n) = c2;;d \<and> (c1, S n) \<rightarrow> (c2, S (Suc n))) \<or> (\<exists>k. C k = SKIP;;d)"
+apply (induction c)
+sorry
+
+  
 end
